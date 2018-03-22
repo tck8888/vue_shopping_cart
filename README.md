208a4a4dd5e4d3709640aaa7afc26f63c41ab8aa
tck8888 <380054728@qq.com>
2018-03-22 23:20:04 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# vue_shopping_cart
+# vue购物车
+
+## 安装vue环境
+
+    1. npm install
+    2. npm install vue --save
+    3. npm install vue-resource --save
---
+++ js/cart.js
@@ -0,0 +1,16 @@
+var vm = new Vue({
+    el: "app",
+    data: {
+        title: "hello world",
+    },
+    filters: {
+
+    },
+    mounted: function () {
+
+    },
+
+    methods: {
+
+    }
+});
